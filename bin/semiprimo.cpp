a92dd2e6a33241ed5002d3fd3b4f329cafec26b0
DavideC03 <davide.castellani.5.8.03@gmail.com>
2021-05-05 18:44:48 +0200

    Local test ok

--- bin/semiprimo.cpp
+++ bin/semiprimo.cpp
@@ -19,24 +19,51 @@
 using namespace std;
 
 // Variabiles
-int N;
+int N, n, divisors, d1, d2;
 
 // Main code
 int main()
 {
-  // Cncomment the following lines if you want to read/write from files
-  // freopen("input.txt", "r", stdin);
-  // freopen("output.txt", "w", stdout);
+    // Cncomment the following lines if you want to read/write from files
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
 
-  // Input
-  cin >> N;
+    // Input
+    cin >> N;
+    n = N;
+    divisors = 0;
+    size_t i;
 
-  // Code
-  // ...
+    for (i = 2; i < N / 2; ++i)
+    {
+        if (n % i == 0)
+        {
+            switch (divisors)
+            {
+            case 0:
+                divisors = 1;
+                d1 = i;
+                break;
+            case 1:
+                divisors = 2;
+                d2 = i;
+                break;
+            case 2:
+                cout << -1 << endl;
+                return 0;
+            }
 
-  // Output
-  cout << N << endl;
+            n /= i;
+            --i;
+        }
+    }
 
-  // End
-  return 0;
+    // Output
+    if (divisors == 2)
+        cout << d1 << " " << d2 << endl;
+    else
+        cout << -1 << endl;
+
+    // End
+    return 0;
 }
